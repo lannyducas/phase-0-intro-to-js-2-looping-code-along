541723e33ac5f8ae10d5ef07c4662b3d26335150
Danny <lannyducas@gmail.com>
2022-07-19 22:12:58 -0400

    commit tree 097ee243b284ea15ee20925f31b0d0df9e275afd

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+function writeCards(array, event){
+    const thanks = [];
+    for (let i=0; i<array.length; i++){
+        thanks[i] = `Thank you, ${array[i]}, for the wonderful ${event} gift!`;
+    }
+return thanks;
+}
+function countDown(num){
+    for(let i = 0; i<=num;i++){
+        console.log(num-i);
+    }
+}
